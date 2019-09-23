b369942dbc05094e0cab336e98e81dec2b85a7bd
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-09-23 10:03:45 +0200

    Print data and make an action plan

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rmData:[]
+    }
+  }
+
+  componentDidMount() {
+    const endpoint = 'https://rickandmortyapi.com/api/character/';
+
+    fetch(endpoint)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          rmData:data.results
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.rmData.map(item => {
+          return(
+            <ul className="character-list">
+              <li className="character-element">
+                <div className="image-container">
+                  <img src={item.image} alt={`Imagen de ${item.name}`} className="character__image"/>
+                </div>
+                <h2 className="character__name">{item.name}</h2>
+                <p className="character__specie">{item.species}</p>
+              {/* <div>{item.name}</div> */}
+              </li>
+            </ul>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
