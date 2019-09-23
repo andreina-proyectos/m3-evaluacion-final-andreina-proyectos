b5838c59ff33701685532d870ec5940feadebc74
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-09-23 15:53:38 +0200

    Add details on detail card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,10 +5,10 @@ const CharacterCard = (props) => {
   return(
     <div className="card">
       <div className="image-container">
-              <img src={img} alt={`Imagen de ${name}`} className="character__image"/>
-            </div>
-            <h2 className="character__name">{name}</h2>
-            <p className="character__specie">{species}</p>
+        <img src={img} alt={`Imagen de ${name}`} className="character__image"/>
+      </div>
+      <h2 className="character__name">{name}</h2>
+      <p className="character__specie">{species}</p>
     </div>
   )
 }
--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,12 +3,31 @@ import {Link} from 'react-router-dom';
 
 
 const CharacterDetail = props => {
-  const {rmData,query} = props;
+  const {rmData, routerProps} = props;
+  console.log(routerProps);
+  const routerPropId = parseInt(routerProps.match.params.id);
+  console.log(routerPropId);
+  const cardClicked = rmData.find(item => {
+    return(
+      item.id === routerPropId
+    )
+  })
+
+  console.log(cardClicked);
+  const {name, image, species, status, origin, episode} = cardClicked;
     return (
         <React.Fragment>
-            <div className="character-detail__container"> Soy el detalle </div>
-            <h2 className="character__name">{rmData.name}</h2>  
+          <div className="character-detail__container">
+            <div className="image-container">
+              <img src={image} alt={`Imagen de ${name}`} className="character__image"/>
+            </div>
+            <h2 className="character__name">{name}</h2>
+            <p className="character__status">{status}</p>  
+            <p className="character__specie">{species}</p>  
+            <p className="character__origin">{origin.name}</p>
+            <p className="character__origin">{episode.length}</p>  
             <Link to="/" className="link__go-back">Go back!</Link>
+          </div>
         </React.Fragment>
     );
 };
