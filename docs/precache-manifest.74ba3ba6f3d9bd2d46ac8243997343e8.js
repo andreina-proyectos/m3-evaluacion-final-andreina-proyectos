02b1722dfaa45cc70be17b517f7bfc918acab83e
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-09-24 11:05:46 +0200

    Run build docs

---
+++ docs/precache-manifest.74ba3ba6f3d9bd2d46ac8243997343e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad02fc3810bf671705f7cb2c8bf0f918",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ce2054b709785dcffb7",
+    "url": "./static/css/main.ce41be57.chunk.css"
+  },
+  {
+    "revision": "6e786548dfa8cff0ae01",
+    "url": "./static/js/2.d743cff3.chunk.js"
+  },
+  {
+    "revision": "7ce2054b709785dcffb7",
+    "url": "./static/js/main.b383502e.chunk.js"
+  },
+  {
+    "revision": "15a7aedd7ca037bdfbae",
+    "url": "./static/js/runtime-main.b217b4bd.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
